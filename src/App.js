bc61c5037d606455f5904312de1d2df7f23b1def
Eric Engman <eengman19@gmail.com>
2020-12-10 17:59:01 -0800

    styling and forms added, basic search and console logs. Spotify Web API now requires access tokens so cannot query, don't feel like registering the app for access

--- src/App.js
+++ src/App.js
@@ -1,14 +1,53 @@
 import React, { Component } from 'react';
+import './App.css';
+import { FormGroup, FormControl, InputGroup, Button } from 'react-bootstrap';
 
 class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            query: ''
+        }
+    }
+
+
+
+    search() {
+        console.log('this.state', this.state);
+        const BASE_URL = 'https://api.spotify.com/v1/search?';
+        const FETCH_URL = BASE_URL + 'q=' + this.state.query + '&type=artist&limit=1';
+        console.log('FETCH_URL', FETCH_URL);
+    }
+
     render() {
         return (
-            <div>
-                <div className="App-title"> Music master</div>
-                <div>
-                    <input placeholder="search for an artist..." />
-                    <button>button</button>
+            <div className="App">
+                <div className="App-title">
+                    <h1> Music master </h1>
                 </div>
+                <FormGroup className="SearchGroup">
+                    <InputGroup>
+
+                        <FormControl
+                            type="text"
+                            placeholder="Search for an Artist"
+                            query={this.state.query}
+                            onChange={event => { this.setState({ query: event.target.value }) }}
+                            onKeyPress={event => {
+                                if (event.key === 'Enter') {
+                                    this.search()
+                                }
+                            }}
+                        />
+                    </InputGroup>
+
+                    <Button
+                        onClick={() => this.search()}
+
+                    >
+                        Submit</Button>
+
+                </FormGroup>
                 <div className="Profile">
                     <div>Artist Picture</div>
                     <div>Artist Name</div>
