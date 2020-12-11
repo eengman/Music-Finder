5ff0eb9d7cbaf9fccde0a2786e99c9d014f18954
Eric Engman <eengman19@gmail.com>
2020-12-10 17:10:08 -0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+    render() {
+        return (
+            <div>
+                <div className="App-title"> Music master</div>
+                <div>
+                    <input placeholder="search for an artist..." />
+                    <button>button</button>
+                </div>
+                <div className="Profile">
+                    <div>Artist Picture</div>
+                    <div>Artist Name</div>
+                </div>
+                <div className="Gallery">
+                    Gallery
+                </div>
+            </div>
+        )
+    }
 }
 
 export default App;
